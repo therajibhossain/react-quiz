5420f19241668e8c5383e70ca446d5fe5cc79663
Rajib Hossain <rajib.hossain@rmgnetworks.com>
2021-12-16 18:54:23 +0600

    lesson-7 source code push

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,10 @@
 import "../styles/App.css";
 import Layout from "./Layout";
-import Quiz from "./pages/Quiz";
+// import Quiz from "./pages/Quiz";
 // import Login from "./pages/Login";
 // import Home from "./pages/Home";
 // import Signup from "./pages/Signup";
+import Result from "./pages/Result";
 
 function App() {
   return (
@@ -11,7 +12,8 @@ function App() {
       {/* <Home /> */}
       {/* <Signup /> */}
       {/* <Login /> */}
-      <Quiz />
+      {/* <Quiz /> */}
+      <Result />
     </Layout>
   );
 }
